9692d7d2c79d5105a42250661522cf0c985d7d1b
powerjaehoonzz <powerjaehoon@gmail.com>
2026-01-11 23:50:03 +0900

    feat: add default page handling for root path

--- node/main.js
+++ node/main.js
@@ -6,10 +6,10 @@ var app = http.createServer(function (req, res) {
   var _url = req.url;
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
-  var title = queryData.id;
-
   if (pathname === "/") {
-    fs.readFile(`../data/${queryData.id}`, "utf8", (err, description) => {
+    if (queryData.id === undefined) {
+      var title = "Welcome";
+      var description = "Hello, Node.js";
       var template = `
       <!doctype html>
       <html>
@@ -31,7 +31,32 @@ var app = http.createServer(function (req, res) {
       `;
       res.writeHead(200);
       res.end(template);
-    });
+    } else {
+      fs.readFile(`../data/${queryData.id}`, "utf8", (err, description) => {
+        var title = queryData.id;
+        var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ol>
+            <li><a href="/?id=html">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ol>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `;
+        res.writeHead(200);
+        res.end(template);
+      });
+    }
   } else {
     res.writeHead(404);
     res.end("Not found");
